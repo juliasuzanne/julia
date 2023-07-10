c300af40c49682d3bc2575edae2c02b7ff7c5839
Julia Grimes <julia.s.grimes@gmail.com>
2023-07-10 14:47:04 -0400

    Added description to Unity

--- src/UnityGame.jsx
+++ src/UnityGame.jsx
@@ -9,5 +9,17 @@ export function UnityGame() {
     codeUrl: "/src/assets/TestSpace/Build/TestSpace.wasm.gz",
   });
 
-  return <Unity unityProvider={unityProvider} />;
+  return (
+    <div>
+      <p className="fonted">
+        {""}I am taking a Udemy course trying to learn to code and use Unity. This is one of the sample games. When I
+        have some free time, I'd love to customize the graphics and sounds. I used this as an opportunity to play
+        serializing data using JSON, which took a while but I finally figured out. There are some unpolished features.
+        Stay tuned for updates :).
+        {""}
+      </p>
+      <p className="fonted"> Press "P" to pause </p>
+      <Unity unityProvider={unityProvider} />
+    </div>
+  );
 }
