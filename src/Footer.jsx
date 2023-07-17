850340dd794b97615681c660ba462ef209a86334
Julia Grimes <julia.s.grimes@gmail.com>
2023-07-17 00:33:34 -0400

    Changing images

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -17,7 +17,10 @@ export function Footer() {
     <div>
       <footer id="footer">
         <button onClick={handleShowConversation}>
-          <img height="200px" src="src/assets/counterJulia.gif" />
+          <img
+            height="200px"
+            src="https://res.cloudinary.com/dytb4ayqj/image/upload/v1689568349/counterJulia_eh7ehf.gif"
+          />
         </button>
         <ConversationModal show={isConversationVisible} onClose={handleCloseConversation}>
           <TextAdventure onClose={handleCloseConversation} />
--- src/Header.jsx
+++ src/Header.jsx
@@ -2,7 +2,10 @@ export function Header() {
   return (
     <header className="header">
       <a href="/">
-        <img className="sig-img" src="/src/assets/sign_julia.png" />
+        <img
+          className="sig-img"
+          src="https://res.cloudinary.com/dytb4ayqj/image/upload/v1689568361/sign_julia_u4bp5c.png"
+        />
         <p className="sig-head"> DESIGNER • PROGRAMMER • ARTIST</p>
       </a>
     </header>
