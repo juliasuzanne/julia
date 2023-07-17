483cd515de013ceb91b8e5399d421723fdaa9da4
Julia Grimes <julia.s.grimes@gmail.com>
2023-07-17 01:15:20 -0400

    Moved test space folder into public

--- src/UnityGame.jsx
+++ src/UnityGame.jsx
@@ -3,10 +3,10 @@ import { Unity, useUnityContext } from "react-unity-webgl";
 
 export function UnityGame() {
   const { unityProvider } = useUnityContext({
-    loaderUrl: "/src/assets/TestSpace/Build/TestSpace.loader.js",
-    dataUrl: "/src/assets/TestSpace/Build/TestSpace.data.gz",
-    frameworkUrl: "/src/assets/TestSpace/Build/TestSpace.framework.js.gz",
-    codeUrl: "/src/assets/TestSpace/Build/TestSpace.wasm.gz",
+    loaderUrl: "/TestSpace/Build/TestSpace.loader.js",
+    dataUrl: "/TestSpace/Build/TestSpace.data.gz",
+    frameworkUrl: "/TestSpace/Build/TestSpace.framework.js.gz",
+    codeUrl: "/TestSpace/Build/TestSpace.wasm.gz",
   });
 
   return (
