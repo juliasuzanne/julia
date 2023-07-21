6577f31a43b462c16bbeaa36e21e5197395287ac
Julia Grimes <julia.s.grimes@gmail.com>
2023-07-21 12:07:37 -0400

    added to description

--- src/UnityGame.jsx
+++ src/UnityGame.jsx
@@ -11,11 +11,11 @@ export function UnityGame() {
   return (
     <div>
       <p className="fonted">
-        {""}I am taking a Udemy course trying to learn to code and use Unity. This is one of the sample games. The art
-        is all custom. Please enjoy!
+        {""}I am taking a Udemy course trying to learn to code and use Unity. This is my take on GameDevHQ's shooter
+        project. The art is all custom. Please enjoy!
         {""}
       </p>
-      <p className="fonted"> Press "P" to pause </p>
+      <p className="fonted"> Press "P" to pause, game may take a minute to load : ) </p>
       <Unity unityProvider={unityProvider} />
     </div>
   );
