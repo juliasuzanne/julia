1e32e578ff216dd09895ec5b4991f975582aedbd
Julia Grimes <julia.s.grimes@gmail.com>
2023-07-21 10:57:42 -0400

    Updated game on unity page

--- src/UnityGame.jsx
+++ src/UnityGame.jsx
@@ -2,19 +2,17 @@ import { Unity, useUnityContext } from "react-unity-webgl";
 
 export function UnityGame() {
   const { unityProvider } = useUnityContext({
-    loaderUrl: "/TestSpace/Build/TestSpace.loader.js",
-    dataUrl: "/TestSpace/Build/TestSpace.data.gz",
-    frameworkUrl: "/TestSpace/Build/TestSpace.framework.js.gz",
-    codeUrl: "/TestSpace/Build/TestSpace.wasm.gz",
+    loaderUrl: "/Dream2/Build/Dream2.loader.js",
+    dataUrl: "/Dream2/Build/Dream2.data.gz",
+    frameworkUrl: "/Dream2/Build/Dream2.framework.js.gz",
+    codeUrl: "/Dream2/Build/Dream2.wasm.gz",
   });
 
   return (
     <div>
       <p className="fonted">
-        {""}I am taking a Udemy course trying to learn to code and use Unity. This is one of the sample games. When I
-        have some free time, I'd love to customize the graphics and sounds. I used this as an opportunity to play
-        serializing data using JSON, which took a while but I finally figured out. There are some unpolished features.
-        Stay tuned for updates :).
+        {""}I am taking a Udemy course trying to learn to code and use Unity. This is one of the sample games. The art
+        is all custom. Please enjoy!
         {""}
       </p>
       <p className="fonted"> Press "P" to pause </p>
