9c962a32aa318a70e22d804a881107cd0852aaab
Julia Grimes <julia.s.grimes@gmail.com>
2023-07-21 12:17:17 -0400

    Add description to CNC box

--- src/Paintings.jsx
+++ src/Paintings.jsx
@@ -2,10 +2,10 @@ export function Paintings() {
   return (
     <div>
       <div>
-        <h2 className="heading" id="center-div-painting">
+        <h1 className="heading" id="center-div-painting">
           {" "}
           Oil Paintings{" "}
-        </h2>
+        </h1>
         <br></br>
         <p className="fonted" id="center-div-painting">
           {" "}
@@ -19,6 +19,14 @@ export function Paintings() {
       <div className="paintings_container">
         <img width="80%" src="https://res.cloudinary.com/dytb4ayqj/image/upload/v1689569244/paintings_issjv6.jpg" />
       </div>
+      <h1 className="heading" id="center-div-painting">
+        {" "}
+        CNC Milled Box{" "}
+      </h1>
+      <p className="fonted" id="center-div-painting">
+        {" "}
+        Custom program to mill this detailed muscular face on a brass box, 3 x 2 x 1.5 inches{" "}
+      </p>
       <img width="80%" src="https://res.cloudinary.com/dytb4ayqj/image/upload/v1689569253/cnc_zhxtmk.jpg" />
     </div>
   );
