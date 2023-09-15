36b058b78ffc166221170f71a4079eced6dd2a89
Julia Grimes <julia.s.grimes@gmail.com>
2023-09-15 12:01:55 -0400

    Fix tarot, remove frontend interface to backend, change header colors

--- src/NewCard.jsx
+++ src/NewCard.jsx
@@ -4,10 +4,18 @@ export function NewCard() {
   const handleSubmit = (event) => {
     event.preventDefault();
     const params = new FormData(event.target);
-    axios.post("https://cleantarot.fly.dev/cards", params).then((response) => {
+    axios.post("https://tarot.fly.dev/cards", params).then((response) => {
       console.log(response.data);
-      event.target.reset();
-      window.location.href = "/"; // Change this to hide a modal, redirect to a specific page, etc.
+      // event.target.reset();
+      // window.location.href = "/newtarotcard"; // Change this to hide a modal, redirect to a specific page, etc.
+    });
+  };
+
+  const handleUpdate = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    axios.patch("https://tarot.fly.dev/cards/7", params).then((response) => {
+      console.log(response.data);
     });
   };
 
@@ -15,6 +23,23 @@ export function NewCard() {
     <div id="newcard">
       <h1>New Card</h1>
       <form onSubmit={handleSubmit}>
+        <div>
+          Name: <input name="name" type="text" />
+        </div>
+        <div>
+          Image: <input name="image" type="text" />
+        </div>
+        <div>
+          Description: <input name="description" type="text" />
+        </div>
+        <div>
+          Category: <input name="category" type="text" />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+
+      <h1>Update Card</h1>
+      <form onSubmit={handleUpdate}>
         <div>
           Name: <input name="name" type="text" />
         </div>
