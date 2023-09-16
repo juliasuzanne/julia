3e8ad5b7ca5b0c47f123e34a89ba4709a7eb929f
Julia Grimes <julia.s.grimes@gmail.com>
2023-09-15 21:05:09 -0400

    Playing with work journal

--- src/WorkJournal.jsx
+++ src/WorkJournal.jsx
@@ -7,7 +7,21 @@ export function WorkJournal() {
       </h2>
       <br></br>
       <br></br>
-      <h3> Week 1</h3>
+      <div id="footerWeeks">
+        <a href="#week1" id="weekButton">
+          Week 1
+        </a>
+        <a href="#week2" id="weekButton">
+          Week 2
+        </a>
+        <a href="#week3" id="weekButton">
+          Week 3
+        </a>
+      </div>
+      <br></br>
+      <br></br>
+
+      <h3 id="week1"> Week 1</h3>
       <p>Goals:</p>
       <p>
         {" "}
@@ -15,14 +29,20 @@ export function WorkJournal() {
         do not have the extensive experience that would make me extremely confident in achieving the end product I want
         in the amount of time that I want.
       </p>
-      <h3>Week 2</h3>
+      <h3 id="week2">Week 2</h3>
       <p> This week I created a new character, started building the background.</p>
       <br></br>
-      <h3>Week 3</h3>
+      <h3 id="week3">Week 3</h3>
       <p> So far this week I have: </p>
       <ul>
-        <li>Animated the character I created last week.</li>
-        <li></li>
+        Added camera, cinemachine// virtual camera// add extension, confiner, separate bounds empty game object Started
+        building mask While animating person Knowing where the skirt meets, had to add a third piece of skirt, will have
+        to edit it later from the individual frames Cuphead Started optimizing code, using abstract inheriting classes
+        Virtual and protected, abstract Working on transcribing The Tain by The Decemberists to work on soundtrack
+        Creating images for scroller game Editing background picture Updated personal website, fixed CORS issue with
+        tarot cards, Started pinterest board Working on scroller game Work through creating elements from first scene
+        plan Debug and finesse foundational code Create dialog maps and character animations for interactions with
+        dialog Animate main character and build shadow self (done 9/13) Navmesh for click walking style
       </ul>
     </div>
   );
