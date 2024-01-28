09ebb8206f5f02a4e5ba6b2fbdb5cbcb23a64280
Julia Grimes <julia.s.grimes@gmail.com>
2024-01-28 16:07:10 -0500

    add description

--- src/Home.jsx
+++ src/Home.jsx
@@ -30,8 +30,12 @@ export function Home() {
             src="https://res.cloudinary.com/dytb4ayqj/image/upload/w_300,f_auto/v1689568546/jacket_qu5w8p.png"
           />
           <div className="img__description_layer">
-            <p className="img__description">fabric and sewing</p>
+            {" "}
+            <p className="img__description">
+              miscellaneous projects that are more process oriented, journalling what I learn along the way
+            </p>{" "}
           </div>
+          <p id="description"> misc projects</p>
         </a>
       </div>
       <div className="img__wrap">
@@ -41,8 +45,9 @@ export function Home() {
             src="https://res.cloudinary.com/dytb4ayqj/image/upload/v1706462377/skeleton_b9ewlh.png"
           />
           <div className="img__description_layer">
-            <p className="img__description">thesis Unity blog</p>
+            <p className="img__description">thesis blog journalling weekly progress</p>
           </div>
+          <p id="description"> work journal</p>
         </a>
       </div>
       <div className="img__wrap">
@@ -51,8 +56,10 @@ export function Home() {
             className="main-page-buttons"
             src="https://res.cloudinary.com/dytb4ayqj/image/upload/v1706462933/website_ybkjv7.png"
           />
+          <p id="description">web dev</p>
+
           <div className="img__description_layer">
-            <p className="img__description">web development</p>
+            <p className="img__description">websites I've made </p>
           </div>
         </a>
       </div>
@@ -63,8 +70,9 @@ export function Home() {
             src="https://res.cloudinary.com/dytb4ayqj/image/upload/v1706464309/Screen_Shot_2024-01-28_at_12.44_a1nrb1.png"
           />
           <div className="img__description_layer">
-            <p className="img__description">art</p>
+            <p className="img__description">paintings and other visual things I've made</p>
           </div>
+          <p id="description">art</p>
         </a>
       </div>
       <div className="img__wrap">
@@ -73,8 +81,10 @@ export function Home() {
             className="main-page-buttons"
             src="https://res.cloudinary.com/dytb4ayqj/image/upload/v1706462220/taroticon_q9u3ec.png"
           />
+          <p id="description">mini games</p>
+
           <div className="img__description_layer">
-            <p className="img__description">mini-games</p>
+            <p className="img__description"> an assortment of little interactive things for you to play with</p>{" "}
           </div>
         </a>
       </div>
