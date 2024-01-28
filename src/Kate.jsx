c889d60ad37a358a006861c79450773b66194ab6
Julia Grimes <julia.s.grimes@gmail.com>
2024-01-28 14:54:04 -0500

    Update kate page, add some description

--- src/Kate.jsx
+++ src/Kate.jsx
@@ -3,8 +3,19 @@ import { FooterDefault } from "./FooterDefault";
 export function Kate() {
   return (
     <div>
-      <a href="http://kate-fitz.com">Visit her site here</a>
-      <FooterDefault></FooterDefault>
+      <p> Kate wanted a custom site built as an archive of drawings, dreams, and stories.</p>
+      <p>
+        We worked together on mocking up the way it would look, adjusting to make sure it looked good on mobile as well.
+      </p>
+      <p> It's sort of like her own personal Google.</p>
+      <p>
+        {" "}
+        I was honored to develop it for her. It gave me a good chance to practice working with a client, creating an
+        accessible backend for someone else, working with carousels and loading many images at once.
+      </p>
+      <a target="blank" href="http://kate-fitz.com">
+        Visit her site here
+      </a>
     </div>
   );
 }
