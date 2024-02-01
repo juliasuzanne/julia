0b6f98b7aebb78b25b44ae9f53bb058ec4a8eefd
Julia Grimes <julia.s.grimes@gmail.com>
2024-02-01 01:04:16 -0500

    Fix bad under construction

--- src/ContactCircle.jsx
+++ src/ContactCircle.jsx
@@ -4,7 +4,7 @@ import ReactCurvedText from "react-curved-text";
 export function ContactCircle() {
   return (
     <>
-      //{" "}
+      {/* //{" "}
       <div id="footercircle">
         //{" "}
         <div id="circle" className="contactcircle">
@@ -29,8 +29,8 @@ export function ContactCircle() {
             //{" "}
           </svg>
           //{" "}
-        </div>
-        {/* <svg viewBox="0 0 425 300">
+        </div> */}
+      {/* <svg viewBox="0 0 425 300">
         <path
           id="curve"
           d="M6,150C49.63,93,105.79,36.65,156.2,47.55,207.89,58.74,213,131.91,264,150c40.67,14.43,108.57-6.91,229-145"
@@ -39,8 +39,7 @@ export function ContactCircle() {
           <textPath href="#curve">Dangerous Curves Ahead</textPath>
         </text>
       </svg> */}
-        //{" "}
-      </div>
+      {/* </div> */}
     </>
   );
 }
