242c61a295e7f08115029cd2ea70646ea5acd256
Julia Grimes <julia.s.grimes@gmail.com>
2024-01-31 22:46:00 -0500

    Remove svg box blocking links

--- src/ContactCircle.jsx
+++ src/ContactCircle.jsx
@@ -5,20 +5,18 @@ export function ContactCircle() {
   return (
     <div id="footercircle">
       <div id="circle" className="contactcircle">
-        <a href="/">
-          <svg version="1.1" x="0px" y="0px" width="100px" height="100px" viewBox="0 0 200 350">
-            <defs>
-              <path id="circlePath" d=" M 200, 200 m -60, 0 a 60,60 0 0,1 120,0 a 60,60 0 0,1 -120,0 " />
-            </defs>
-            <g>
-              <use href="#circlePath" fill="none" />
-              <rect x="160" y="160" width="80" height="80" fill="transparent" />
-              <text fill="red">
-                <textPath href="#circlePath">PAGE*UNDER*CONSTRUCTION*</textPath>
-              </text>
-            </g>
-          </svg>
-        </a>
+        <svg version="1.1" x="0px" y="0px" width="100px" height="100px" viewBox="0 0 200 350" pointerEvents="none">
+          <defs>
+            <path id="circlePath" d=" M 200, 200 m -60, 0 a 60,60 0 0,1 120,0 a 60,60 0 0,1 -120,0 " />
+          </defs>
+          <g>
+            <use href="#circlePath" fill="none" />
+            <rect x="160" y="160" width="80" height="80" fill="transparent" />
+            <text fill="red">
+              <textPath href="#circlePath">PAGE*UNDER*CONSTRUCTION*</textPath>
+            </text>
+          </g>
+        </svg>
       </div>
 
       {/* <svg viewBox="0 0 425 300">
