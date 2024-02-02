0d1aaba70dd2cbd6d48fa2b976327bf3e3b568a6
Julia Grimes <julia.s.grimes@gmail.com>
2024-02-01 20:38:55 -0500

    Adjust subtitle

--- src/RIYL/Riyl.jsx
+++ src/RIYL/Riyl.jsx
@@ -19,8 +19,11 @@ export function Riyl() {
 
   return (
     <div>
-      <h2> Media Consumption</h2>
-      <h4> Keeping record of all the media I consume while creating my thesis project FA23-SP24</h4>
+      <h2 className="title"> Media Consumption</h2>
+      <h5 className="subtitle">
+        {" "}
+        Keeping record of all the media I consume while creating my thesis project FA23-SP24
+      </h5>
       <br />
       <br />
       <RiylIndex posts={posts} />
