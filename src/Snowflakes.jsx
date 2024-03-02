45de47341331086359372639edf4f05f9b75120e
Julia Grimes <julia.s.grimes@gmail.com>
2024-03-02 10:53:49 -0500

    change snowflake and header photo:

--- src/Snowflakes.jsx
+++ src/Snowflakes.jsx
@@ -11,21 +11,16 @@ export function Snowflakes() {
     "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/963a0f84-83bf-4d19-a442-3648efd7574a/dacmqt2-f2da178a-495c-49f9-9316-36bb5e1b4284.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzk2M2EwZjg0LTgzYmYtNGQxOS1hNDQyLTM2NDhlZmQ3NTc0YVwvZGFjbXF0Mi1mMmRhMTc4YS00OTVjLTQ5ZjktOTMxNi0zNmJiNWUxYjQyODQucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.gIAJH5JdxbjaQ4aPAZ9CVWgSX8_4Pv7E6u2Z4UQSv8A";
   const pencil = document.createElement("img");
   pencil.src = "https://jackkneukergraphicdesign.files.wordpress.com/2020/03/impossibleshape1.png?w=1024";
-  const blank1 = document.createElement("img");
-  pencil.src =
-    "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASwAAACoCAMAAABt9SM9AAAAA1BMVEX///+nxBvIAAAAR0lEQVR4nO3BAQEAAACCIP+vbkhAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAO8GxYgAAb0jQ/cAAAAASUVORK5CYII=";
-  const blank2 = document.createElement("img");
-  pencil.src =
-    "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASwAAACoCAMAAABt9SM9AAAAA1BMVEX///+nxBvIAAAAR0lEQVR4nO3BAQEAAACCIP+vbkhAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAO8GxYgAAb0jQ/cAAAAASUVORK5CYII=";
-  const images = [boot, glove, umbrella, pencil, blank1, blank2];
+
+  const images = [boot, glove, umbrella, pencil];
 
   return (
     <div>
       <Snowfall
         // Changes the snowflake color
         radius={[200.0, 300.0]}
-        speed={[5, 10]}
-        wind={[10.0, 25]}
+        speed={[2, 6]}
+        wind={[2.0, 9]}
         // Applied to the canvas element
         style={{ zIndex: "2", height: "205vh", position: "fixed" }}
         images={images}
