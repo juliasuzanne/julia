32218b44d3b1825d7c5d299f1fe8888415e09afc
Julia Grimes <julia.s.grimes@gmail.com>
2024-05-17 00:08:38 -0400

    trying again with new path

--- src/ThesisArtWalk.jsx
+++ src/ThesisArtWalk.jsx
@@ -1,12 +1,12 @@
 import { Unity, useUnityContext } from "react-unity-webgl";
 
 export function ThesisArtWalk() {
-  // const { unityProvider } = useUnityContext({
-  //   loaderUrl: "/SignsOfLifeWEBGL/Build/SignsOfLifeWEBGL.loader.js",
-  //   dataUrl: "/SignsOfLifeWEBGL/Build/SignsOfLifeWEBGL.data",
-  //   frameworkUrl: "/SignsOfLifeWEBGL/Build/SignsOfLifeWEBGL.framework.js",
-  //   codeUrl: "/SignsOfLifeWEBGL/Build/SignsOfLifeWEBGL.wasm",
-  // });
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "/SignsOfLifeWEBGL/Build/SignsOfLifeWEBGL.loader.js",
+    dataUrl: "/SignsOfLifeWEBGL/Build/SignsOfLifeWEBGL.data",
+    frameworkUrl: "/SignsOfLifeWEBGL/Build/SignsOfLifeWEBGL.framework.js",
+    codeUrl: "/SignsOfLifeWEBGL/Build/SignsOfLifeWEBGL.wasm",
+  });
 
   return (
     <div>
@@ -34,7 +34,7 @@ export function ThesisArtWalk() {
       <br></br>
       <br></br> <br></br>
       <br></br>
-      {/* <Unity unityProvider={unityProvider} /> */}
+      <Unity unityProvider={unityProvider} />
       <h3 className="title">About</h3>
       <p>
         This game represents a time of reflection and reconnection with myself. After taking an eight year hiatus
