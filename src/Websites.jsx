408bb7e4057d6d39eb3a41d3ae18c709c8684aba
Julia Grimes <julia.s.grimes@gmail.com>
2024-05-17 21:28:58 -0400

    import useeffect

--- src/Websites.jsx
+++ src/Websites.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import Carousel from "react-bootstrap/Carousel";
 import { ContactCircle } from "./ContactCircle";
+import { useEffect } from "react";
 
 export function Websites() {
   useEffect(() => {
