8f8efb6e7e59c5194356255715a9cc8a17c49477
Julia Grimes <julia.s.grimes@gmail.com>
2024-05-31 12:07:48 -0400

    Formatting/style

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,6 +6,7 @@ export function Navbar() {
       &nbsp;
       <a href="/">
         <img
+          className="navbaricon"
           src="      https://res.cloudinary.com/dytb4ayqj/image/upload/v1711601270/jglogoswim_gecmj4.png
           "
           height="30vw"
@@ -38,19 +39,20 @@ export function Navbar() {
       </a> */}
       &nbsp; &nbsp;
       <a href="https://www.linkedin.com/in/julia-grimes" rel="noreferrer" target="_blank">
-        <img src="https://cdn-icons-png.flaticon.com/512/1384/1384014.png" height="30vw"></img>
+        <img className="navbaricon" src="https://cdn-icons-png.flaticon.com/512/1384/1384014.png" height="30vw"></img>
       </a>
       &nbsp; &nbsp; &nbsp;
       <a href="https://github.com/juliasuzanne" rel="noreferrer" target="_blank">
-        <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" height="30vw" />
+        <img className="navbaricon" src="https://cdn-icons-png.flaticon.com/512/25/25231.png" height="30vw" />
       </a>
       &nbsp; &nbsp;
       <a href="https://pin.it/1nO9KwdHp" rel="noreferrer" target="_blank">
-        <img src="https://cdn-icons-png.flaticon.com/512/1051/1051329.png" height="30vw" />
+        <img className="navbaricon" src="https://cdn-icons-png.flaticon.com/512/1051/1051329.png" height="30vw" />
       </a>
       &nbsp; &nbsp;
       <a href="mailto: julia.s.grimes@gmail.com">
         <img
+          className="navbaricon"
           src="https://cdn1.iconfinder.com/data/icons/business-office-and-finance/128/If_00033_enveloppe_mail-512.png"
           height="30vw"
         />
@@ -70,13 +72,13 @@ export function Navbar() {
       <div className="collapse navbar-collapse" id="navbarNavDropdown">
         <ul className="navbar-nav">
           <li className="nav-item active">
-            <a className="nav-link" href="/">
+            <a id="whitetext" className="nav-link" href="/">
               Home <span className="sr-only"></span>
             </a>
           </li>
 
           <li className="nav-item active">
-            <a className="nav-link" href="/paintings">
+            <a id="whitetext" id="whitetext" className="nav-link" href="/paintings">
               Art <span className="sr-only"></span>
             </a>
           </li>
@@ -85,14 +87,14 @@ export function Navbar() {
             <a
               className="nav-link dropdown-toggle"
               href="#"
-              id="navbarDropdownMenuLink"
+              id="whitetext"
               data-bs-toggle="dropdown"
               aria-haspopup="true"
               aria-expanded="false"
             >
               Web Development
             </a>
-            <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <div className="dropdown-menu" aria-labelledby="whitetext">
               <a className="dropdown-item" href="/kate">
                 Kate Fitz Art
               </a>
@@ -115,14 +117,14 @@ export function Navbar() {
             <a
               className="nav-link dropdown-toggle"
               href="#"
-              id="navbarDropdownMenuLink"
+              id="whitetext"
               data-bs-toggle="dropdown"
               aria-haspopup="true"
               aria-expanded="false"
             >
               Mini-Games
             </a>
-            <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <div className="dropdown-menu" aria-labelledby="whitetext">
               <a className="dropdown-item" href="/tarot">
                 Tarot
               </a>
@@ -139,14 +141,14 @@ export function Navbar() {
             <a
               className="nav-link dropdown-toggle"
               href="#"
-              id="navbarDropdownMenuLink"
+              id="whitetext"
               data-bs-toggle="dropdown"
               aria-haspopup="true"
               aria-expanded="false"
             >
               Game Dev
             </a>
-            <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <div className="dropdown-menu" aria-labelledby="whitetext">
               <a className="dropdown-item" href="thesisblog">
                 Weekly Progress
               </a>
