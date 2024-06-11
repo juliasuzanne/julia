f08e13aa9c3ae2332442abfb55701b84f5cd9f0c
Julia Grimes <julia.s.grimes@gmail.com>
2024-06-11 12:16:38 -0400

    Fix footer

--- src/SimpleFooter.jsx
+++ src/SimpleFooter.jsx
@@ -1,11 +1,9 @@
 export function SimpleFooter() {
   return (
     <div className="simpleFooter">
-      <p>
-        <a href="https://www.linkedin.com/in/julia-grimes/">LinkedIn</a>
-        <br></br>
-        ©2024 Julia Grimes All Rights Reserved
-      </p>
+      <a href="https://www.linkedin.com/in/julia-grimes/">LinkedIn</a> &emsp;
+      <a href="https://github.com/juliasuzanne"> Github</a>
+      <p className="leftjustify"> ©2024 Julia Grimes All Rights Reserved </p>
     </div>
   );
 }
