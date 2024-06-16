5051ace45ee130bbb7611ac49adf0350c89628d7
Julia Grimes <julia.s.grimes@gmail.com>
2024-06-16 13:02:50 -0400

    Update resume

--- src/SimpleFooter.jsx
+++ src/SimpleFooter.jsx
@@ -4,7 +4,7 @@ export function SimpleFooter() {
       <p>
         <a href="https://www.linkedin.com/in/julia-grimes/">LinkedIn</a> &emsp;
         <a href="https://github.com/juliasuzanne"> Github</a>&emsp;
-        <a className="nav-link active" aria-current="page" href="/Grimes_Resume.pdf" download>
+        <a className="nav-link active" aria-current="page" href="/JGrimes_Resume.pdf" download>
           CV
         </a>
         &emsp; &emsp; ©2024 Julia Grimes All Rights Reserved
