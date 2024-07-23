3fc5cf324cbaf8d13412804dfea91702248346ce
juliasuzanne <julia.s.grimes@gmail.com>
2024-07-23 08:42:34 -0400

    Update content on game page

--- src/ThesisArtWalk.jsx
+++ src/ThesisArtWalk.jsx
@@ -43,20 +43,6 @@ export function ThesisArtWalk() {
         description={currentDescription}
         close={handleHideModal}
       ></ImagesModal>
-      <p className="topStatement">
-        {""}I'm proud to present the version of my thesis project, Signs Of Life -
-        <strong> a Unity built 2D point and click game </strong>- I showed at MICA's 2024 Art Walk. This game is still
-        in progress, but playable with an ending. Please enjoy and <strong>stay tuned </strong>for the final, published
-        version on Steam!
-      </p>
-      <br></br>
-      <h2 className="heading">Trailer of Game in Development</h2>
-      <div id="videogame">
-        <iframe className="websitevideogame" controls="1" src="/soundflowers_1.mp4" title="video player"></iframe>
-      </div>
-      <br></br>
-      <br></br>
-
       <div className="artwalkdemoimage-container">
         <img
           onClick={() => {
@@ -115,34 +101,19 @@ export function ThesisArtWalk() {
           src="     https://res.cloudinary.com/dytb4ayqj/image/upload/v1715300992/Screen_Shot_2024-05-09_at_8.29.35_PM_noiqsi.png"
         ></img>
       </div>
-
+      <p className="topStatement">
+        {""}I'm proud to present the version of my thesis project, Signs Of Life -
+        <strong> a Unity built 2D point and click game </strong>- I showed at MICA's 2024 Art Walk. This game is still
+        in progress, but playable with an ending. Please enjoy and <strong>stay tuned </strong>for the final, published
+        version on Steam!{" "}
+        <a href="/thesisblog">
+          <strong>(I kept a blog throughout the process of making this game, read it by clicking here.)</strong>
+        </a>
+      </p>
+      <br></br>
+      <br></br>
       <div className="container">
         <div className="row">
-          <div className="col-lg-6">
-            {/* <Unity unityProvider={unityProvider} /> */}
-            <h3 className="title">About</h3>
-            <br></br>
-            <a href="/thesisblog">
-              <p className="aboutPBold">
-                (I kept a blog throughout the process of making this game, read it by clicking here.)
-              </p>
-            </a>
-
-            <p className="aboutP">
-              This game represents a time of reflection and reconnection with myself. After taking an eight year hiatus
-              between dropping out and coming back to finish my Bachelor's degree, I have a different outlook on art and
-              my relationship to that creative side of myself. It was very important to me to have this outlet in my
-              childhood and adolescence, and I'd like to try to find a place for it in my adult life while still
-              incorporating a lot of the skills and fulfillment I get from more technical work that I practice now.{" "}
-            </p>
-            <p className="aboutP">
-              This game includes many writings, drawings, and ideas from the time in my life when I first left school -
-              when games were very important to a sense of connection in a world that felt increasingly difficult to
-              navigate and exist in. I hope that it facilitates a meditative, controlled environment for your thoughts
-              as those games did for me, and a sense of discovery as you explore and interact with the many items and
-              characters.
-            </p>
-          </div>
           <div className="col-lg-6">
             <h2 className="title">Play</h2>
             <br></br>
@@ -151,46 +122,72 @@ export function ThesisArtWalk() {
         Game may take a few minutes to load... thank you for your patience. Tip: game will play better on a larger
         screen/full size window and is not mobile friendly.
       </p> */}
+            <p className="aboutPBold"> Signs Of Life is now available to play as a google drive link.</p>
+            <a href="https://drive.google.com/file/d/1VkluueJSzTx5twagtPzmzdv4l91RRXGC/view?usp=sharing">
+              <h4 className="postheader"> Download - For Mac</h4>
+            </a>
+            <a href="https://drive.google.com/file/d/1xkv-5Udq6FVqYMKYx-SbZXtNGFiVXUKJ/view?usp=sharing">
+              <h4 className="postheader"> Download - For PC</h4>
+            </a>
+            <br></br>
+          </div>
+          <div className="col-lg-6">
             <div className="borderNewspaper">
-              <p className="aboutPBold"> Signs Of Life is now available to play as a google drive link.</p>
-              <a href="#additional">
-                <p className="aboutPBold"> Scroll down or click here to see additional mini games. </p>
-              </a>
-              <a href="https://drive.google.com/file/d/1VkluueJSzTx5twagtPzmzdv4l91RRXGC/view?usp=sharing">
-                <h4 className="postheader"> Download SOL - For Mac</h4>
-              </a>
-              <a href="https://drive.google.com/file/d/1xkv-5Udq6FVqYMKYx-SbZXtNGFiVXUKJ/view?usp=sharing">
-                <h4 className="postheader"> Download SOL - For PC</h4>
-              </a>
+              <h2 className="title">Other Games</h2>
               <br></br>
-              <img
-                className="showimage"
-                src="https://res.cloudinary.com/dytb4ayqj/image/upload/v1717687388/showimage_ciw9mu.png"
-              ></img>
-              <p>(Above) Image of game installation in Art Walk gallery at MICA May 2024.</p>
+              <a href="/tarot">
+                <p className="aboutPBold">Tarot Card Mimic built with React and Ruby on Rails</p>
+              </a>
+              <a href="/snake">
+                <p className="aboutPBold">Snake Mimic built with HTML/React and JS</p>
+              </a>
+              <a href="/unity">
+                <p className="aboutPBold">Shooter Style Mini Game Built with Unity</p>
+              </a>
+              <a href="/draft">
+                <p className="aboutPBold">Point and Click Demo</p>
+              </a>
+              {/* <Unity unityProvider={unityProvider} /> */}
             </div>
           </div>
-          {/* <br></br>
+          <br></br>
           <br></br> <br></br>
-          <br></br> */}
+          <br></br>
         </div>
       </div>
-      <div className="col-lg-12" id="additional">
-        <h2 className="title">Additional Games</h2>
-        <br></br>
-        <a href="/tarot">
-          <p className="aboutPBold">Tarot Card Mimic built with React and Ruby on Rails</p>
-        </a>
-        <a href="/snake">
-          <p className="aboutPBold">Snake Mimic built with HTML/React and JS</p>
-        </a>
-        <a href="/unity">
-          <p className="aboutPBold">Shooter Style Mini Game Built with Unity</p>
-        </a>
-        <a href="/draft">
-          <p className="aboutPBold">Unity Experiment</p>
-        </a>
+      <br></br> <br></br>
+      <br></br>
+      <br></br>
+      <h2 className="heading">Trailer of Game in Development</h2>
+      <div id="videogame">
+        <iframe className="websitevideogame" controls="1" src="/soundflowers_1.mp4" title="video player"></iframe>
       </div>
+      <div className="col-lg-8" id="additional">
+        <h3 className="title">About</h3>
+
+        <p className="aboutP">
+          This game represents a time of reflection and reconnection with myself. After taking an eight year hiatus
+          between dropping out and coming back to finish my Bachelor's degree, I have a different outlook on art and my
+          relationship to that creative side of myself. It was very important to me to have this outlet in my childhood
+          and adolescence, and I'd like to try to find a place for it in my adult life while still incorporating a lot
+          of the skills and fulfillment I get from more technical work that I practice now.{" "}
+        </p>
+        <p className="aboutP">
+          This game includes many writings, drawings, and ideas from the time in my life when I first left school - when
+          games were very important to a sense of connection in a world that felt increasingly difficult to navigate and
+          exist in. I hope that it facilitates a meditative, controlled environment for your thoughts as those games did
+          for me, and a sense of discovery as you explore and interact with the many items and characters.
+        </p>
+      </div>
+      {/* <p>(Below) Image of game installation in Art Walk gallery at MICA May 2024.</p>
+      <img
+        className="showimageMICA"
+        src="https://res.cloudinary.com/dytb4ayqj/image/upload/v1717687388/showimage_ciw9mu.png"
+      ></img> */}
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
     </div>
   );
 }
