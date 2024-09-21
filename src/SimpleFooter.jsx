4188197b1ce0bf2345ef99dd441220ec20dd5e18
juliasuzanne <julia.s.grimes@gmail.com>
2024-09-21 16:12:51 -0400

    Update resume

--- src/SimpleFooter.jsx
+++ src/SimpleFooter.jsx
@@ -5,7 +5,7 @@ export function SimpleFooter() {
         <a href="/contact">Contact</a> &emsp;
         <a href="https://www.linkedin.com/in/julia-grimes/">LinkedIn</a> &emsp;
         <a href="https://github.com/juliasuzanne"> Github</a>&emsp;
-        <a className="nav-link active" aria-current="page" href="/JGrimes_Resume.pdf" download>
+        <a className="nav-link active" aria-current="page" href="/Resume_JSG2024.pdf" download>
           CV
         </a>
         &emsp; &emsp; ©2024 Julia Grimes All Rights Reserved
