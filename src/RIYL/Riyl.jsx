47518d0d873694da873c6cfd9c2e03d7e4667eab
juliasuzanne <julia.s.grimes@gmail.com>
2024-11-02 09:56:30 -0400

    update riyl to use internal db even though it's not visible on site for good housekeeping

--- src/RIYL/Riyl.jsx
+++ src/RIYL/Riyl.jsx
@@ -1,21 +1,10 @@
 import axios from "axios";
 import { RiylIndex } from "./RiylIndex";
-
+import { riylposts } from "./riylposts";
 import { useEffect, useState } from "react";
 
 export function Riyl() {
-  const [posts, setPosts] = useState([]);
-
-  const handleIndexPosts = () => {
-    console.log("handleIndexPosts");
-    axios.get(`https://riyl.fly.dev/infos.json`).then((response) => {
-      console.log(response.data);
-      setPosts([...posts, response.data]);
-      setPosts(response.data);
-    });
-  };
-
-  useEffect(handleIndexPosts(), []);
+  const [posts, setPosts] = useState(riylposts);
 
   return (
     <div>
---
+++ src/RIYL/riylposts.js
@@ -0,0 +1,99 @@
+export const riylposts = [
+  {
+    id: 3,
+    title: "Florence Yeoh",
+    category: "This is a short unity game ",
+    favoritepart:
+      "I found this watching the channel side dish on youtube, it's a short 2D game made on unity. I liked the very simple drawing style and unique mechanics that allow you to kind of intuit or troubleshoot how to move forwards with the story. It also kind of read like an interactive comic strip. The story was emotional and centered on a transition in the main character's life and an important relationship. I like it, it was immersive and felt calm and quiet. Even though it was something I did on the computer, it recalled a simpler time for me when I didn't feel glued to my phone and was more focused on friendships and what was right in front of me. I liked the unique drawing style, it kind of reminded me of Madeline.",
+    author: "Mountains",
+    created_at: "2023-10-16T03:53:02.447Z",
+    updated_at: "2023-11-01T03:26:37.610Z",
+    photo_url:
+      "https://cdn.akamai.steamstatic.com/steam/apps/1102130/ss_fe7d6ef1f2bf34692e29f8650c432df1908f914b.1920x1080.jpg?t=1687368401",
+  },
+  {
+    id: 1,
+    title: "Opal",
+    category:
+      "This is a short stop-motion film by Jack Stauber on Adult Swim. It's about 12 minutes long and is about a little girl navigating her complicated and damaged home life.",
+    favoritepart:
+      "I found this surfing Youtube. The story is sort of related to what I'm trying to do. I really liked the loud punchy voices and songs. I liked the portrayal of her fantasy within the billboard across her house, and how nothing is explained. I also liked how each family member sort of had their own moment.",
+    author: "Jack Stauber",
+    created_at: "2023-10-13T01:51:10.393Z",
+    updated_at: "2023-10-13T03:15:42.985Z",
+    photo_url: "https://i.ytimg.com/vi/7Oo-E12BP0M/hqdefault.jpg",
+  },
+  {
+    id: 5,
+    title: "Sisters With Transistors",
+    category: "This is a documentary about women in electronic music",
+    favoritepart:
+      "I watched this for class, at first I felt uninterested in having to watch it because I was worried based off the title and the subject that it would be this kind of self congratulatory and somewhat fake feminism that I am so sick of since the Barbie movie came out, but it was actually quite good. I liked some of the sounds and it made me see sound and music differently. I love 70s sci fi and never thought about the history of some of these weird sounds like the Dr. Who theme song. I also liked seeing these women just getting their hands dirty, totally engaged with what they were doing. I would recommend it to anyone. It was a theme with the Art21 shorts we watched in class where these artists would come across as off or 'weird' or they would lose partners and money and everything most people would consider core to their life for the sake of their vision, and that interested me, but it that theme played itself out in a different way in this movie that I could relate to more, where it's more about faith and an intrinsic desire to work with some process or material than what could come across more as stubbornness or weirdness for the sake of topping what's been done.",
+    author: "Lisa Rovner",
+    created_at: "2023-10-16T05:39:45.043Z",
+    updated_at: "2023-10-16T05:39:45.043Z",
+    photo_url:
+      "https://freight.cargo.site/w/1200/i/9ca20a0b24e01f74afd123278e71a024959a82d34fa82970f9511f87c3f8ce81/suzanne.jpg",
+  },
+  {
+    id: 6,
+    title: "Coraline",
+    category:
+      "This is a stop motion movie about a girl who learns to accept her new life with her parents in a small town after going through a surreal experience in a parallel universe.",
+    favoritepart:
+      'I re-watched this when they played it at the Coolidge Corner theater. It struck me how simple but meaningful the themes and story arc was, it inspired me to edit my own thoughts down. The eye motif and the way it illustrates the "careful what you wish for" type of mentality was very powerful and beautifully done. I\'m a Neil Gaiman fan as well.',
+    author: "",
+    created_at: "2023-11-01T03:21:32.737Z",
+    updated_at: "2023-11-01T03:21:32.737Z",
+    photo_url:
+      "https://img.buzzfeed.com/buzzfeed-static/static/2019-10/11/0/asset/5e06287a13a6/sub-buzz-1146-1570753314-1.png?downsize=900:*&output-format=auto&output-quality=auto",
+  },
+  {
+    id: 7,
+    title: "Keep Breathing",
+    category:
+      "This is a short series about a woman having a cathartic and introspective experience that changes her life.",
+    favoritepart:
+      "I really appreciated the slow, calm, well paced rhythm of this show. It was refreshing the bouts of silence and focused narrative in the sea of overwhelmingly complicated and loud/flashing types of shows that seem to me are popular to make right now. It was simple but not for the sake of it, it felt genuinely a translation of emotion.",
+    author: "",
+    created_at: "2023-11-01T03:24:54.534Z",
+    updated_at: "2023-11-01T03:24:54.534Z",
+    photo_url: "https://i.ytimg.com/vi/BGKwkYHd7VM/maxresdefault.jpg",
+  },
+  {
+    id: 2,
+    title: "Fantastic Planet",
+    category: "This is an animated French movie from the 1970s about humans living as pets to an alien species.",
+    favoritepart:
+      "I really love the aesthetic and alien world in this movie. I also like seeing the humans make discoveries and operate in conditions different from our reality on earth. The sounds are interesting too.",
+    author: "",
+    created_at: "2023-10-13T02:46:18.612Z",
+    updated_at: "2023-11-01T03:27:11.546Z",
+    photo_url:
+      "https://i0.wp.com/corruptedrecord.com/wp-content/uploads/2016/07/vlcsnap-2014-06-08-19h54m33s231.png?w=364&h=364&crop=1&ssl=1",
+  },
+  {
+    id: 4,
+    title: "Related",
+    category:
+      "This is a point and click style 2D game. It focuses on the psychological torment of a woman who is visiting an abandoned orphanage where she grew up, and recalling memories through this.",
+    favoritepart:
+      'The art style of the game really appeals to me, it has that shaded/illustrative sort of oil-painting or pastel sort of blendy look, but very crisp, with vintage-y and girly accents. It definitely has some aspects of horror and psychological thrills. It was interesting to see the game because it\'s only one "chapter" similar to my goals for the year, so it was cool to see how far they took it. It was nice to see it has a lot of positive reviews. I found the navigation style and the inventory a bit cumbersome and not friendly but other than that interested to see where it goes. It kind of reminds me of Fran Bow.',
+    author: "Dariia Babchuk, Andrii Petrov",
+    created_at: "2023-10-16T05:02:47.499Z",
+    updated_at: "2023-11-01T03:31:36.171Z",
+    photo_url: "https://img.itch.zone/aW1hZ2UvNjI5NTQ5LzMzNTk4NDgucG5n/original/pX%2F1Ty.png",
+  },
+  {
+    id: 8,
+    title: "Sally Face",
+    category: "computer game",
+    favoritepart:
+      "I love this game, it's so simple and so immersive. It has a really unique look, I love that they made their own font. It really inspires me because it was built in unity and I believe within my skill level to have programmed. One of my favorite games. Something I can learn from it is how simple yet consistent and entertaining it is.",
+    author: "Portable Moose",
+    created_at: "2023-12-21T18:36:21.556Z",
+    updated_at: "2023-12-21T18:36:21.556Z",
+    photo_url:
+      "https://cdn.akamai.steamstatic.com/steam/apps/541570/ss_3f1c3d3278b99e25da857bcf3ad0ef65cbd58b1a.1920x1080.jpg?t=1667000571",
+  },
+];
